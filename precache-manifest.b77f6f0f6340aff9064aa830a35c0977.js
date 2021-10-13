b9b904cede9af4291cbcd65325a4f3d1c7eb5900
Chan Zilin <zilin@repromarketing.com>
2021-10-13 15:55:28 +0800

    Updates

---
+++ precache-manifest.b77f6f0f6340aff9064aa830a35c0977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480550e17324fd5a23994d830691fac6",
+    "url": "/Chatbot/index.html"
+  },
+  {
+    "revision": "7ac0331c6b7a7e222d18",
+    "url": "/Chatbot/static/css/main.7980a18c.chunk.css"
+  },
+  {
+    "revision": "3d8c727f10dcdeb28036",
+    "url": "/Chatbot/static/js/2.9a5592f5.chunk.js"
+  },
+  {
+    "revision": "518dcbf48c4edb7f703816d92798fdc6",
+    "url": "/Chatbot/static/js/2.9a5592f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac0331c6b7a7e222d18",
+    "url": "/Chatbot/static/js/main.35368a96.chunk.js"
+  },
+  {
+    "revision": "163698cd986d08fe64f5",
+    "url": "/Chatbot/static/js/runtime-main.87cf55ac.js"
+  },
+  {
+    "revision": "5b5f61a9022ddf5fd7e028fd4fc4e734",
+    "url": "/Chatbot/static/media/bot.5b5f61a9.jpg"
+  }
+]);
