3babf37ab5f12b51d8565b9834ebf097e951c555
Chan <yatogamikuroh012@gmail.com>
2021-10-28 16:31:02 +0800

    Updates

---
+++ precache-manifest.2d55772b970d4e345bc3134b7bc9170a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf730b85efbf6d7fb0aa6887cae33933",
+    "url": "/Chatbot/index.html"
+  },
+  {
+    "revision": "d1e065155be4e8026221",
+    "url": "/Chatbot/static/css/main.8ba6dca9.chunk.css"
+  },
+  {
+    "revision": "ac110cafda007b41b9ee",
+    "url": "/Chatbot/static/js/2.993a5ec6.chunk.js"
+  },
+  {
+    "revision": "518dcbf48c4edb7f703816d92798fdc6",
+    "url": "/Chatbot/static/js/2.993a5ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e065155be4e8026221",
+    "url": "/Chatbot/static/js/main.71366ea5.chunk.js"
+  },
+  {
+    "revision": "163698cd986d08fe64f5",
+    "url": "/Chatbot/static/js/runtime-main.87cf55ac.js"
+  },
+  {
+    "revision": "5b5f61a9022ddf5fd7e028fd4fc4e734",
+    "url": "/Chatbot/static/media/bot.5b5f61a9.jpg"
+  },
+  {
+    "revision": "665a8aa573dec7333e90bc758427d45c",
+    "url": "/Chatbot/static/media/link.665a8aa5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4ddeb20ad762e2e70971750b7ca55663.js"
+  "/Chatbot/precache-manifest.2d55772b970d4e345bc3134b7bc9170a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Chatbot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
