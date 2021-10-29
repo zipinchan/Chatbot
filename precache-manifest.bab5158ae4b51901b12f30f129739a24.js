82134952c39b08f0aeeb96ac9e50488c39b8c9a3
Chan <yatogamikuroh012@gmail.com>
2021-10-29 21:18:11 +0800

    Updates

---
+++ precache-manifest.bab5158ae4b51901b12f30f129739a24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada37d2064eca6c88497316a056c53d9",
+    "url": "/Chatbot/index.html"
+  },
+  {
+    "revision": "a063c5c78b7b8907de54",
+    "url": "/Chatbot/static/css/main.670a94a0.chunk.css"
+  },
+  {
+    "revision": "fe41a15f46a31e0de7fd",
+    "url": "/Chatbot/static/js/2.445c797b.chunk.js"
+  },
+  {
+    "revision": "eefcf1de2988f9e0bf25685218038938",
+    "url": "/Chatbot/static/js/2.445c797b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a063c5c78b7b8907de54",
+    "url": "/Chatbot/static/js/main.773528e6.chunk.js"
+  },
+  {
+    "revision": "ae5d040c25c6a31ccada",
+    "url": "/Chatbot/static/js/runtime-main.bbce4f2c.js"
+  },
+  {
+    "revision": "8f2c1ebdc9aaa20b91ffa71ae367de54",
+    "url": "/Chatbot/static/media/ab1.8f2c1ebd.png"
+  },
+  {
+    "revision": "e00a07f5cc8e50dac76d5845a8b04e89",
+    "url": "/Chatbot/static/media/ab2.e00a07f5.jpg"
+  },
+  {
+    "revision": "5b5f61a9022ddf5fd7e028fd4fc4e734",
+    "url": "/Chatbot/static/media/bot.5b5f61a9.jpg"
+  },
+  {
+    "revision": "665a8aa573dec7333e90bc758427d45c",
+    "url": "/Chatbot/static/media/link.665a8aa5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.834c353ead63521e9b1f7c7feee95633.js"
+  "/Chatbot/precache-manifest.bab5158ae4b51901b12f30f129739a24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Chatbot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
